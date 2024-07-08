0d51f346b23c8596a8052eec277ca18936f369a1
AMNA WAQAR <amnawaqar110@gmail.com>
2024-07-08 20:33:01 +0100

    step 5

--- FizzLibrary/Shared/Service/FizzBuzzService.cs
+++ FizzLibrary/Shared/Service/FizzBuzzService.cs
@@ -14,15 +14,29 @@ namespace FizzLibrary.Shared.Service
             var fizzBuzz = new List<string>();
             for (int i = 1; i <= 1000; i++)
             {
-                if (i % 3 == 0 && i % 5 == 0)
-                    fizzBuzz.Add("fizzbuzz");
-                else if (i % 3 == 0)
-                    fizzBuzz.Add("fizz");
-                else if (i % 5 == 0)
-                    fizzBuzz.Add("buzz");
+                if (DateTime.Today.DayOfWeek == DayOfWeek.Wednesday)
+                {
+                    if (i % 3 == 0 && i % 5 == 0)
+                        fizzBuzz.Add("wizzwuzz");
+                    else if (i % 3 == 0)
+                        fizzBuzz.Add("wizz");
+                    else if (i % 5 == 0)
+                        fizzBuzz.Add("wuzz");
+                    else
+                        fizzBuzz.Add(i.ToString());
+                }
                 else
-                    fizzBuzz.Add(i.ToString());
-            }
+                {
+                    if (i % 3 == 0 && i % 5 == 0)
+                        fizzBuzz.Add("fizzbuzz");
+                    else if (i % 3 == 0)
+                        fizzBuzz.Add("fizz");
+                    else if (i % 5 == 0)
+                        fizzBuzz.Add("buzz");
+                    else
+                        fizzBuzz.Add(i.ToString());
+                }
+        }
             return fizzBuzz;
         }
     }
